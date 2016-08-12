dc24a2ce5662ea57898ba90a17bec32b65d44ac0
Callum May <callumdmay@gmail.com>
2016-08-11 22:40:51 -0600

    improve UI with bootstrap

--- app/controllers/weatherController.js
+++ app/controllers/weatherController.js
@@ -3,7 +3,6 @@ angular.module('notificationApp.weatherController', []).
 controller('weatherController', function($scope, $http, $timeout) {
 
 
-  $scope.weatherData= "No Weather Data Available";
   // Function to get the data
   $scope.getForecast = function(){
     $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?id=5913490&units=metric&&APPID=f9dbd911bc01df1d9ce563b2ba4d3209')
@@ -18,8 +17,6 @@ controller('weatherController', function($scope, $http, $timeout) {
       $scope.currentWeatherData=angular.fromJson(response.data);
     });
   };
-
-
 
   // Function to replicate setInterval using $timeout service.
   $scope.intervalFunction = function(){
