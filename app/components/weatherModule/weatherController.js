793cbb4bcc19dfdacc2fba037ce12d9d1179b6ad
Callum May <callumdmay@gmail.com>
2016-10-16 21:27:34 -0400

    Add temporary weather query counter

--- app/components/weatherModule/weatherController.js
+++ app/components/weatherModule/weatherController.js
@@ -1,14 +1,17 @@
 angular.module('notificationApp.controllers').
 controller('weatherController', function($scope, $interval, $timeout, $location, weatherFactory) {
 
+    $scope.queryCount = 0;
     $scope.updateCurrentWeather = function() {
         weatherFactory.getCurrentWeather().then(function(response) {
+            $scope.queryCount++;
             $scope.currentWeatherData = response.data;
         });
     };
 
     $scope.updateForecast = function() {
         weatherFactory.getForecast().then(function(response) {
+            $scope.queryCount++;
             $scope.forecastWeatherData = response.data;
         });
     };
